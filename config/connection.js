6a05fb9579cfc2498aa3441a24e4b522b8e74b9a
nick adams <thebigdeal916@icloud.com>
2020-04-05 19:15:51 -0700

    v1 server

--- config/connection.js
+++ config/connection.js
@@ -8,8 +8,8 @@ if (process.env.JAWSDB_URL) {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "password",
-    database: "burger_db"
+    password: "Theking1@",
+    database: "js_scheduler_db"
   });
 }
 
