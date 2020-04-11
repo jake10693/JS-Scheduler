7eadf806da9f4ea598093ea8fd0673c24a5fe53f
Yeasir H <yeasir01@gmail.com>
2020-04-11 16:01:10 -0700

    build get call to populate select box

--- public/js/function.js
+++ public/js/function.js
@@ -1,63 +1,36 @@
-$(document).ready(function () {
+$(document).ready(function() {
+    $("#save-1").click(function() {
+      var newName = $("#newName")
+        .val()
+        .trim();
 
-    $("#save-1").click(function () {
-
-        var newName = $("#newName").val().trim()
-
-        $.ajax({
-            url: "/api/employee",
-            method: "POST",
-            data: {
-                name: newName
-            }
-        }).then(function (data) {
-            clearText()
-            console.log(data)
-        })
-    })
+      $.ajax({
+        url: "/api/employee",
+        method: "POST",
+        data: {
+          name: newName
+        }
+      }).then(function(data) {
+        clearText();
+        console.log(data);
+      });
+    });
 
     function clearText() {
-        $("#newName").val("")
+      $("#newName").val("");
     }
 
     $.ajax({
-        url: "/api/employee",
-        method: "GET"
-    }).then(function (data) {
+      url: "/api/employee",
+      method: "GET"
+    }).then(function(data) {
+      $("#employee-select").empty();
 
-        $('#employee-select')
-            .find('option')
-            .remove()
+      for (var i = 0; i < data.length; i++) {
+        let name = data[i].name;
+        $("#employee-select").append($(`<option value=${name}>${name}</option>`));
+      }
 
-
-        for (var i = 0; i > data.length; i++) {
-            $("#employee-select").append($("<option>").text(data[i].name))
-        };
-
-        console.log(data);
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-})
+      $("#employee-select").formSelect();
+    });
+});
