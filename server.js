5b654075fcdb24090187ad65c141416858f52676
Yeasir H <yeasir01@gmail.com>
2020-04-15 12:09:42 -0700

    force build table

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ require("./routes/api-routes.js")(app)
 // ===============================================================
 // Drop & Rebuild Tables use {force: true}
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({force: true}).then(function () {
   app.listen(PORT, function () {
     console.log("==> Listening on port %s. Visit http://localhost:%s in your browser", PORT, PORT)
   });
